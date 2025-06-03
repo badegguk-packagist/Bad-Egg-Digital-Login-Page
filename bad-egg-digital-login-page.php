1ee00b1c90b46d08934d252f5e8e20cda5981230
Steve Ross <steve@ross.wtf>
2025-06-03 16:19:29 +0100

    replace composer autoloader

--- bad-egg-digital-login-page.php
+++ bad-egg-digital-login-page.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Bad Egg Digital Login Page
  * Plugin URI:        https://www.badegg.uk
  * Description:       Applies Bad Egg Digital Branding elements to the Wordpress login page.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Requires at least: 5.2
  * Requires PHP:      8.1
  * Author:            Bad Egg Digital
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if(!defined('BEDLP')) define('BEDLP', 'bad-egg-digital-login-page');
-if(!defined('BEDLP_VER')) define('BEDLP_VER', '1.0.1');
+if(!defined('BEDLP_VER')) define('BEDLP_VER', '1.0.2');
 if(!defined('BEDLP_BASENAME')) define('BEDLP_BASENAME', plugin_basename(__FILE__));
 if(!defined('BEDLP_FILE')) define('BEDLP_FILE', __FILE__);
 if(!defined('BEDLP_DIR')) define('BEDLP_DIR', plugin_dir_path( __FILE__ ));
